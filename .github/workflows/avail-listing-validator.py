5077c7c225bf3de2a873617e318a0f5721c212f3
Anukkrit Shanker <anukkrit.official@gmail.com>
2024-01-27 00:59:21 +0530

    Create avail-listing-validator.py

---
+++ .github/workflows/avail-listing-validator.py
@@ -0,0 +1,93 @@
+import json
+import requests
+import sys
+
+JSON_URL = "https://raw.githubusercontent.com/karnotxyz/avail-campaign-listing/main/listing.json"
+
+
+def validate_json_array(json_file):
+    with open(json_file, 'r') as file:
+        listing = json.load(file)
+
+    if not isinstance(listing, list):
+        print(f"Error: The JSON file {json_file} does not contain a JSON array.")
+        sys.exit(1)
+    return listing
+
+
+def download_json_file(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: Failed to download JSON file from {url}.")
+
+
+def check_required_keys(obj):
+    required_keys = ["id", "name", "logo", "rpc_url", "explorer_url", "metrics_endpoint"]
+    for key in required_keys:
+        if key not in obj:
+            print(f"Error: The JSON file does not contain a {key} field.")
+            sys.exit(1)
+
+
+def check_url_status_code(obj):
+    try:
+        if "rpc_url" in obj:
+            response = requests.head(obj["rpc_url"] + "/health")
+            if response.status_code != 200:
+                print(f"Error: The RPC URL {obj['rpc_url']} is not accessible.")
+                sys.exit(1)
+        else:
+            print(f"Error: The JSON file does not contain a rpc_url field.")
+            sys.exit(1)
+
+        if "explorer_url" in obj:
+            response = requests.head(obj["explorer_url"])
+            if response.status_code != 200:
+                print(f"Error: The Explorer URL {obj['explorer_url']} is not accessible.")
+                sys.exit(1)
+        else:
+            print(f"Error: The JSON file does not contain a explorer_url field.")
+            sys.exit(1)
+
+        if "metrics_endpoint" in obj:
+            response = requests.head(obj["metrics_endpoint"] + "/metrics")
+            if response.status_code != 200:
+                print(f"Error: The Metrics URL {obj['metrics_endpoint']} is not accessible.")
+                sys.exit(1)
+        else:
+            print(f"Error: The JSON file does not contain a metrics_endpoint field.")
+            sys.exit(1)
+    except Exception as e:
+        print(f"Error: URL not working - {e.args[0].reason.args[0]}")
+
+
+def check_duplicate_urls_in_latest_entry(main_json, latest_entry):
+    RPC_URLS = []
+    METRICS_URLS = []
+    EXPLORER_URLS = []
+    for entry in main_json:
+        RPC_URLS.append(entry["rpc_url"])
+        METRICS_URLS.append(entry["metrics_endpoint"])
+        EXPLORER_URLS.append(entry["explorer_url"])
+
+    if latest_entry["rpc_url"] in RPC_URLS:
+        print(f"Error: The RPC URL {latest_entry['rpc_url']} is already present in the JSON file.")
+        sys.exit(1)
+    if latest_entry["metrics_endpoint"] in METRICS_URLS:
+        print(f"Error: The metrics_endpoint  {latest_entry['metrics_endpoint']} is already present in the JSON file.")
+        sys.exit(1)
+    if latest_entry["explorer_url"] in EXPLORER_URLS:
+        print(f"Error: The Explorer URL {latest_entry['explorer_url']} is already present in the JSON file.")
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    new_entry = validate_json_array(sys.argv[1])
+    latest_entry = new_entry[len(new_entry) - 1]
+    check_required_keys(obj=latest_entry)
+    check_url_status_code(obj=latest_entry)
+    data = download_json_file(JSON_URL)
+    check_duplicate_urls_in_latest_entry(data, latest_entry)
+
