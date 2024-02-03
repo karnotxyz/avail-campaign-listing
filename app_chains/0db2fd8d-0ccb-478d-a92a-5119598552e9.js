b3c030a5625ef96ed21347ce610747081d2cef70
Moeen <moeen@Moeens-Mac-mini.local>
2024-02-03 22:05:12 +0300

    ✨ Adding berrylab

---
+++ app_chains/0db2fd8d-0ccb-478d-a92a-5119598552e9.js
@@ -0,0 +1,8 @@
+{
+    "name": "berrylab",
+    "logo": "https://static.berrylab.cloud/BrandKit/PNG/BerryLabLogo_Main.png",
+    "rpc_url": "http://madara.berrylab.cloud:9094",
+    "explorer_url": "http://madara.berrylab.cloud:4000",
+    "metrics_endpoint": "http://madara.berrylab.cloud:9615/metrics",
+    "id": "0db2fd8d-0ccb-478d-a92a-5119598552e9"
+  }
