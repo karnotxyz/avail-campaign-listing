ca954990198c65a68c986fdf06d76c31e03cdf02
ETHSev <95754429+ETHSev@users.noreply.github.com>
2024-02-07 18:02:58 +0100

    Add files via upload

---
+++ app_chains/b03f4964-833b-49fb-a8bf-b3279578d31d.js
@@ -0,0 +1,8 @@
+{
+    "name": "NodeTET",
+    "logo": "https://i.ibb.co/9s7PT8J/Profil.jpg",
+    "rpc_url": "http://158.220.118.205:9944",
+    "explorer_url": "http://158.220.118.205:4000",
+    "metrics_endpoint": "http://158.220.118.205:9615/metrics",
+    "id": "b03f4964-833b-49fb-a8bf-b3279578d31d"
+  }
